bf79f460ed4999a861c1ee571f25f479f904beeb
pnl0966w <Avatar1976!>
2020-12-22 11:20:52 +0100

    initial commit

---
+++ src/test/java/garageClients/clients.feature
@@ -0,0 +1,6 @@
+Feature: First Test Clients
+
+  Background:
+    * url 'localhost:8080'
+
+  Scenario: Call Clients Api and assert they a available
---
+++ src/test/karate-config.js
@@ -0,0 +1,25 @@
+function fn() {
+  var env = karate.env; // get system property 'karate.env'
+  karate.log('karate.env system property was:', env);
+  if (!env) {
+    env = 'dev';
+  }
+  var config = {
+//    env: env,
+
+  };
+
+  if (env == 'dev') {
+    // customize
+    // e.g. config.foo = 'bar';
+
+
+  } else if (env == 'e2e') {
+    // customize
+  }
+
+//    Testing configFile
+    karate.configure('connectTimeout', 5000);
+    karate.configure('readTimeout', 5000);
+  return config;
+}
